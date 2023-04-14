5aa23b5472617f0ccb00cfbbacf95ba25ca248bc
Najeebullah <Najeebullah@DESKTOP-K5N4NQ2>
2023-04-14 20:04:04 +0500

    chat app

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,26 +1,41 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { ChatService } from '../services/chat.service';
+import { StreamInvocationMessage } from '@microsoft/signalr';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { PrivateChatComponent } from '../private-chat/private-chat.component';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
 
   @Output() closeChatEmitter = new EventEmitter();
 
 
-  constructor(public chatService: ChatService) {
+  constructor(public _chatService: ChatService, private modelService: NgbModal) {
 
+  }
+  ngOnDestroy(): void {
+    this._chatService.stopChatConnection();
   }
 
   ngOnInit(): void {
-    this.chatService.createChatConnection();
+    this._chatService.createChatConnection();
   }
 
 
   backToHome() {
     this.closeChatEmitter.emit();
+  }
+
+  sendMessage(content: string) {
+    this._chatService.sendMessage(content);
+  }
+
+  openPrivateChat(toUser: string) {
+    const modelRef = this.modelService.open(PrivateChatComponent);
+    modelRef.componentInstance.toUser = toUser;
   }
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,9 +1,12 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, NgModuleRef } from '@angular/core';
 import { environment } from 'src/environments/environment.ts';
 import { User } from '../models/user';
 import { HubConnection } from '@microsoft/signalr';
 import { HubConnectionBuilder } from '@microsoft/signalr/dist/esm/HubConnectionBuilder'
+import { Message } from '../models/message';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { PrivateChatComponent } from '../private-chat/private-chat.component';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +14,12 @@ import { HubConnectionBuilder } from '@microsoft/signalr/dist/esm/HubConnectionB
 export class ChatService {
   myName: string = '';
   onlineUsers: string[] = [];
+  messages: Message[] = [];
+  privateMessages: Message[] = [];
   private chatConnection?: HubConnection;
-  constructor(private httpClient: HttpClient) { }
+  privateMessageInitiated = false;
+
+  constructor(private httpClient: HttpClient, private modelService: NgbModal) { }
 
   registerUser(user: User) {
     return this.httpClient.post(`${environment.apiUrl}api/chat/register-user`, user, { responseType: 'text' });
@@ -25,13 +32,38 @@ export class ChatService {
       console.log(error)
     })
 
-    this.chatConnection.on('UserConnected', (onlineUsers) => {
-      this.onlineUsers =[...onlineUsers];
-    })
-
-    this.chatConnection.on('OnlineUsers', () => {
+    this.chatConnection.on('UserConnected', () => {
       this.addUserConnectionId();
     })
+
+    this.chatConnection.on('OnlineUsers', (onlineUsers) => {
+      this.onlineUsers = [...onlineUsers];
+    })
+
+    this.chatConnection.on('newMessage', (newMessage: Message) => {
+      this.messages = [...this.messages, newMessage]
+    })
+
+    this.chatConnection.on('OpenPrivateChat', (newMessage: Message) => {
+      this.privateMessages = [...this.privateMessages, newMessage]
+      this.privateMessageInitiated = true;
+      const modelRef = this.modelService.open(PrivateChatComponent)
+      modelRef.componentInstance.toUser = newMessage.from;
+
+    })
+
+    this.chatConnection.on('NewPrivateMessage', (newPrivateMessage: Message) => {
+      this.privateMessages = [...this.privateMessages, newPrivateMessage]
+    })
+
+    this.chatConnection.on('ClosePrivateChat', (newMessage: Message) => {
+      this.privateMessages = []
+      this.privateMessageInitiated = false;
+      this.modelService.dismissAll()
+
+
+    })
+
   }
 
   stopChatConnection() {
@@ -42,4 +74,39 @@ export class ChatService {
     return this.chatConnection?.invoke('AddUserConnectionId', this.myName)
       .catch(error => { console.log(error) });
   }
+
+  async sendMessage(content: string) {
+    const message: Message = {
+      from: this.myName,
+      content: content
+    }
+    return this.chatConnection?.invoke('ReceiveMessage', message)
+      .catch(error => console.log(error));
+  }
+
+  async sendPrivateMessage(to: string, content: string) {
+    const message: Message = {
+      from: this.myName,
+      content: content,
+      to: to
+    }
+    if (!this.privateMessageInitiated) {
+      this.privateMessageInitiated = true;
+      return this.chatConnection?.invoke('CreatePrivateChat', message).then(() => {
+        this.privateMessages = [...this.privateMessages, message]
+      })
+        .catch(error => console.log(error));
+    } else {
+      return this.chatConnection?.invoke('ReceivePrivateMessage', message)
+        .catch(error => console.log(error));
+    }
+
+  }
+
+  async closePrivateChatMessage(otherUser: string) {
+    return this.chatConnection?.invoke('RemovePrivateChat', this.myName, otherUser)
+      .catch(error => console.log(error));
+  }
+
+
 }
